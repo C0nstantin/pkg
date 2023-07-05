bc88012fa4217a3bdec4d4e82d289f0627f03392
Karataev Konstantin <karataev.k.a@gmail.com>
2023-07-06 01:08:02 +0530

    ad types pkg

--- utils/panic.go
+++ utils/panic.go
@@ -4,7 +4,7 @@ import (
 	`log`
 )
 
-// func panic if err input error  panic if err not nil
+// PanicIfErr panic if err not nil
 func PanicIfErr(err error) {
 	if err != nil {
 		panic(err)
@@ -14,15 +14,28 @@ func PanicIfErr(err error) {
 type Closer interface {
 	Close() error
 }
+type Notifier interface {
+	Notify(err error)
+}
 
+// DeferClosePanic close connection if err not nil log error and panic
 func DeferClosePanic(c Closer) {
 	if err := c.Close(); err != nil {
 		log.Panic(err)
 	}
 }
 
+// DeferCloseLog close connection if err not nil log error
 func DeferCloseLog(c Closer) {
 	if err := c.Close(); err != nil {
 		log.Println(err)
+	}
+}
+
+// PanicAndNotify panic if err not nil and notify err
+func PanicAndNotify(err error, notifier Notifier) {
+	if err != nil {
+		notifier.Notify(err)
+		panic(err)
 	}
 }

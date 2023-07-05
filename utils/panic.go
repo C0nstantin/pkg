b4c057f2586628bc8cc50ab7f5b639ddf326bf64
Karataev Konstantin <karataev.k.a@gmail.com>
2023-07-05 19:51:53 +0530

    add defer funcs

--- utils/panic.go
+++ utils/panic.go
@@ -1,8 +1,28 @@
 package utils
 
+import (
+	`log`
+)
+
 // func panic if err input error  panic if err not nil
 func PanicIfErr(err error) {
 	if err != nil {
 		panic(err)
+	}
+}
+
+type Closer interface {
+	Close() error
+}
+
+func DeferClosePanic(c Closer) {
+	if err := c.Close(); err != nil {
+		log.Panic(err)
+	}
+}
+
+func DeferCloseLog(c Closer) {
+	if err := c.Close(); err != nil {
+		log.Println(err)
 	}
 }
